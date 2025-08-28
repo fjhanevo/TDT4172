e9f0d9f1ba685dd76e6414823aeb67915c9bf6c5
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-28 13:53:04 +0200

    added logistic_regression.py

---
+++ Assignment1/src/logistic_regression.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+from linear_regression import LinearRegression
+
+class LogisticRegression(LinearRegression):
+
+    def __init__(self, learning_rate=0.0001, epochs=1000):
+        super().__init__(learning_rate, epochs)
+        self.losses = []
+        self.train_accuracies = []
+
+    @staticmethod
+    def _sigmoid(x):
+        """
+        Sigmoid function 
+        """
+        if x >= 0:
+            z = np.exp(-x)
+            return 1 / (1+z)
+        else: 
+            z = np.exp(x)
+            return z / (1 + z)
+
+    def _compute_gradients(self, X, y, y_pred):
+        """
+        Helper function to compute the two gradients from the loss equation
+        """
+
+        # derivative of the loss function
+        diff = y_pred - y
+        grad_b = np.mean(diff)
+        grad_w = X.T @ diff
+        grad_w = np.array([np.mean(grad) for grad in grad_w])
+
+        return grad_w, grad_b
+
+    def _compute_loss(self, y, y_pred):
+        """
+        Helper function to compute the binary cross entropy
+        """
+
+        y_zero_loss = y * np.log(y_pred + 1e-9)
+        y_one_loss = (1-y) * np.log(1 - y_pred + 1e-9)
+        return - np.mean(y_zero_loss + y_one_loss)
+
+    def fit(self, X, y):
+        """
+        
+        """
+        _ , n_features = X.shape
+
+        # initalize variables
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        # gradient descent loop
+        for _ in range(self.epochs):
+            x_weights = self.w @ X.T + self.b
+            y_pred = np.array([self._sigmoid(val) for val in x_weights])
+            loss = self._compute_loss(y, y_pred)
+            dw, db = self._compute_gradients(X, y, y_pred)
+
+            # update params
+            self.w -= self.learning_rate * dw
+            self.b -= self.learning_rate * db
+
+
+    def predict(self, X):
+        """
+
+        """
+        x_weights =  X @ self.w.T + self.b
+        prob = np.array([self._sigmoid(val) for val in x_weights])
+        return [1 if p > 0.5 else 0 for p in prob]
+
+
